857126c51760aacdbd66c9b591b191a0571b9b99
shivani26v <61066540+shivani26v@users.noreply.github.com>
2020-08-19 13:37:40 +0530

    Update func1.m

--- func1.m
+++ func1.m
@@ -1,10 +1,7 @@
 %given diff equation and input func
 
-    A = [0, 1, 0; 0, 0, 1; -1, -1, -2];
-    B = [0; 0; 1];
-    t0 = 0;
-    dT = 0.1;
-    i = (t-t0)/dT;
-    u = 2 + 3 * sin (0.01 * i);
-    dx = A*x + B*u;
+initial_cond;
+i = (t-t0)/dT;
+u = 2 + 3 * sin (0.01 * i);
+dx = A*x + B*u;
 
