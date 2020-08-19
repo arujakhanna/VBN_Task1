b8f4f382c538712bcacfc93b4c85027b14876ca1
shivani26v <61066540+shivani26v@users.noreply.github.com>
2020-08-19 13:15:12 +0530

    Update and rename dxfunc.m to func1.m

--- func1.m
+++ func1.m
@@ -1,7 +1,5 @@
-%given function
-t=2;
+%given diff equation and input func
 
-function [dx] = f(t,x)
     A = [0, 1, 0; 0, 0, 1; -1, -1, -2];
     B = [0; 0; 1];
     t0 = 0;
@@ -9,5 +7,4 @@ function [dx] = f(t,x)
     i = (t-t0)/dT;
     u = 2 + 3 * sin (0.01 * i);
     dx = A*x + B*u;
-   
-end
+
